794e045edf009bfd433577040f777c13457ad13a
Grey Delamar <grey.delamar@gmail.com>
2021-05-15 15:51:49 -0400

    ✨  use new creep class for upgrader

---
+++ creep.role.upgrader.js
@@ -0,0 +1,25 @@
+var Creep = require("creep");
+
+module.exports = class RoleUpgrader extends Creep {
+  constructor(creep) {
+    super(creep);
+  }
+
+  run() {
+    if (!this.creep.memory.isFull) {
+      this.harvest(true);
+
+      if (
+        this.creep.store.getCapacity() === this.creep.store.getUsedCapacity()
+      ) {
+        this.creep.memory.isFull = true;
+      }
+    } else {
+      this.upgrade();
+
+      if (this.creep.store.getUsedCapacity() === 0) {
+        this.creep.memory.isFull = false;
+      }
+    }
+  }
+};
--- main.js
+++ main.js
@@ -1,62 +1,59 @@
-
-const creepHarvester = require('creep.role.harvester')
-const creepBuilder = require('creep.role.builder')
-const creepRepairer = require('creep.role.repairer')
-var roleUpgrader = require('role.upgrader');
-var spawner = require('spawner');
-var garbageCollector = require('garbageCollector');
+const creepHarvester = require("creep.role.harvester");
+const creepBuilder = require("creep.role.builder");
+const creepRepairer = require("creep.role.repairer");
+const creepUpgrader = require("creep.role.upgrader");
+var spawner = require("spawner");
+var garbageCollector = require("garbageCollector");
 const logger = false;
 
-// console.log(Creep)
-
 module.exports.loop = function () {
-    garbageCollector.run()
-    spawner.run(logger)
+  garbageCollector.run();
+  spawner.run(logger);
 
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
+  var tower = Game.getObjectById("TOWER_ID");
+  if (tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(
+      FIND_STRUCTURES,
+      {
+        filter: (structure) => structure.hits < structure.hitsMax,
+      }
+    );
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
     }
 
-    // console.log('hit here', JSON.stringify(Game.creeps))
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-
-        switch (creep.memory.role) {
-            case 'harvester':
-                const CreepHarvester = new creepHarvester(creep)
-                CreepHarvester.run();
-                break;
-
-            case 'upgrader':
-                roleUpgrader.run(creep);
-                break;
-
-            case 'builder':
-                const CreepBuilder = new creepBuilder(creep)
-                CreepBuilder.run();
-                break;
-
-            case 'repairer':
-                const CreepRepairer = new creepRepairer(creep)
-                CreepRepairer.run();
-                break;
-
-            case 'testDummy':
-                break;
-        }
-
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
     }
-}
+  }
 
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
 
+    switch (creep.memory.role) {
+      case "harvester":
+        const CreepHarvester = new creepHarvester(creep);
+        CreepHarvester.run();
+        break;
+
+      case "upgrader":
+        const CreepUpgrader = new creepUpgrader(creep);
+        CreepUpgrader.run();
+        break;
+
+      case "builder":
+        const CreepBuilder = new creepBuilder(creep);
+        CreepBuilder.run();
+        break;
+
+      case "repairer":
+        const CreepRepairer = new creepRepairer(creep);
+        CreepRepairer.run();
+        break;
+
+      case "testDummy":
+        break;
+    }
+  }
+};
